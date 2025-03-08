ab0ca1fc82ed874a28bed9f1fa7ebb9736de754b
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-03-08 20:18:51 +0530

    Variables and Datatypes

---
+++ Variables_and_Datatypes.py
@@ -0,0 +1,24 @@
+a=complex(8,2)
+b=True
+c="Aditya"
+d=None
+print(a)
+#String datatype always in the " "
+# + will always be used in the same datatype
+a1=34
+print(a+a1)
+print("The type of a is ",type(a))
+print("The type of a is ",type(b))
+print("The type of a is ",type(c))
+print("The type of a is ",type(d))
+#List is a collection of different Datatypes
+list1=[8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+#Tuple is also a collection of different Datatypes But it cannot be modified or Changed
+#It is Immutable
+tuple1=(("parrot","sparrow"),("Lion","Tiger"))
+print(tuple1)
+
+dict1={"name":"Sakshi","age":20,"canVote":True}
+print(dict1)
+# In Python Everything is Object
