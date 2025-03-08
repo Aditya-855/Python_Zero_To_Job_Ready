808a2521c29d8eb4044a2b0989d9dfabe229d76d
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-03-08 14:05:29 +0530

    Comments and Escape Sequence Statements

---
+++ Day5.py
@@ -0,0 +1,15 @@
+# Hey , Please don't Remove this line
+# Author of this Program is Aditya
+# This is a Hundred Day Python Challenge
+print("Hey I am A Good Boy\nand this viewer is also a good Boy\n")
+
+print("Hello World") #This line Prints Hello World
+'''
+Hello World This is a Multi Line Comment in Python 
+
+UnderStand
+
+'''
+print("Hey i m a \"good Boy\"\nand this viewer is also a good boy/girl\n")
+print("Hey",6,7,sep="~",end="0\n")
+print("Aditya")
