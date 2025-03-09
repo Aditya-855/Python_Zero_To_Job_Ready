4acfc0a16c4bd5ec4e1b78fe9db296af5e3b92b4
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-03-09 14:55:05 +0530

    Basic Caculator Function and operators

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+a=9
+b=8
+
+print("The value of",a,"+",b,"is:" , a+b)
+print("The value of",a,"-",b,"is:" , a-b)
+print("The value of",a,"*",b,"is:" , a*b)
+print("The value of",a,"/",b,"is:" , a/b)
+print("The value of",a,"%",b,"is:" , a%b)
+print("The value of",a,"**",b,"is:" , a**b)
