aa2d4c50689fb373fa3f3b1d94403242a55679f6
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-03-17 22:02:47 +0530

    Strings in Python

---
+++ Strings_in_Python.py
@@ -0,0 +1,13 @@
+name="Aditya"
+friend="Virat"
+AnotherFriend="Samar"
+apple='''He said,"Hi Harry 
+Hey Hows this apple
+I want to eat an apple'''
+
+# print("Hello,"+friend)
+# print(apple)
+# print(name[0])
+print("Lets use a for loop\n")
+for character in name:
+    print(character)
