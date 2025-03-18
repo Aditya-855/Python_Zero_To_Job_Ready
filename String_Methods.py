b3ba3114e409ae31fe9354181cc9405c981b11ec
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-03-18 22:48:28 +0530

    Strings Methods in Python

---
+++ String_Methods.py
@@ -0,0 +1,15 @@
+# Strings are immutable
+a="Aditya!!!!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Aditya ","John"))
+print(a.split(" "))
+heading="introduction to python"
+print(heading.capitalize())
+
+str1="Welcome to Console!!!"
+print(len(str1))
+print(len(str1.center(50)))
+print(str1.endswith("!!!"))
