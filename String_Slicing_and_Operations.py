bf13d90eb5145ff9faf9ae2610b313665e3ae6c3
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-03-18 22:48:00 +0530

    String Slicing and Operations in Pyhton

---
+++ String_Slicing_and_Operations.py
@@ -0,0 +1,12 @@
+name="Aditya,Guddu"
+# print(name[0:6])
+# print(len(name))
+fruit="Mango"
+# print(len(fruit))
+# print(fruit[0:4])
+# print(fruit[:4])
+# print(fruit[0:])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
+nm="Harry"
+print(nm[-4:-2])
